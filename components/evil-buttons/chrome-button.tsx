8c250679050452556ca843b0fb14dd862db1279f
Radiumcoders <radiumcoders@gmail.com>
2026-05-22 12:24:04 +0530

    style: enhance ChromeButton with active scaling effect and shadow for improved user interaction

--- components/evil-buttons/chrome-button.tsx
+++ components/evil-buttons/chrome-button.tsx
@@ -3,7 +3,7 @@ import LiquidChrome from "../LiquidChrome";
 
 function ChromeButton({ children }: { children: ReactNode }) {
   return (
-    <button className="relative py-4 px-6 rounded-full border-neutral-900 border-2 bg-neutral-950 overflow-hidden group text-white">
+    <button className="relative py-4 px-6 rounded-full border-neutral-900 border-2 bg-neutral-950 overflow-hidden group text-white active:scale-95 transition-all duration-75 shadow-lg">
       <div className="absolute inset-0 z-0 opacity-80 group-hover:opacity-100 transition-opacity duration-500">
         <LiquidChrome
           baseColor={[
