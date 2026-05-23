71ea8656e772537f09b994994f4a59a0c2c2cbb7
Radiumcoders <radiumcoders@gmail.com>
2026-05-23 11:25:41 +0530

    style: adjust opacity and shadow properties in MinimalButton for better visual consistency

--- components/evil-buttons/minimal.tsx
+++ components/evil-buttons/minimal.tsx
@@ -12,10 +12,10 @@ function MinimalButton({
     <Button
       className={cn(
         className,
-        "relative w-30 h-10 p-0 [--pattern:var(--color-accent)] shadow-[0px_10px_30px_10px_#00000024] dark:shadow-[0px_10px_30px_10px_#ffffff24] border-none",
+        "relative w-30 h-10 p-0 [--pattern:var(--color-accent)] shadow-[0px_10px_30px_10px_#00000024] dark:shadow-[0px_10px_30px_10px_#ffffff10] border-none",
       )}
     >
-      <div className="h-full w-full absolute inset-0 bg-[repeating-linear-gradient(315deg,var(--pattern)_0,var(--pattern)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] opacity-10" />
+      <div className="h-full w-full absolute inset-0 bg-[repeating-linear-gradient(315deg,var(--pattern)_0,var(--pattern)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] opacity-5" />
       <span className="relative z-10">{children}</span>
     </Button>
   );
