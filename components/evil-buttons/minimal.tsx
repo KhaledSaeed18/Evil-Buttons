14b9ad60e2ea9de41da694ec10351db603585e21
Radiumcoders <radiumcoders@gmail.com>
2026-05-23 11:24:05 +0530

    refactor: update MinimalButton component styles for improved layout and consistency

--- components/evil-buttons/minimal.tsx
+++ components/evil-buttons/minimal.tsx
@@ -12,12 +12,11 @@ function MinimalButton({
     <Button
       className={cn(
         className,
-        "relative w-46 h-13 p-0 [--pattern:var(--color-accent)] shadow-[0px_10px_30px_10px_#00000024] dark:shadow-[0px_10px_30px_10px_#ffffff24]",
+        "relative w-30 h-10 p-0 [--pattern:var(--color-accent)] shadow-[0px_10px_30px_10px_#00000024] dark:shadow-[0px_10px_30px_10px_#ffffff24] border-none",
       )}
     >
-      <div className="h-full w-full border-r border-(--pattern) bg-[repeating-linear-gradient(315deg,var(--pattern)_0,var(--pattern)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] opacity-10" />
+      <div className="h-full w-full absolute inset-0 bg-[repeating-linear-gradient(315deg,var(--pattern)_0,var(--pattern)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] opacity-10" />
       <span className="relative z-10">{children}</span>
-      <div className="h-full w-full border-l border-(--pattern) bg-[repeating-linear-gradient(315deg,var(--pattern)_0,var(--pattern)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] opacity-10" />
     </Button>
   );
 }
